ed24c981f3d37d238e6ba4df144bdfcd04bbe635
Jazper Ruedas <jaruedas@amazon.com>
2024-05-28 18:21:03 -0700

    about change

--- pages/About/About.js
+++ pages/About/About.js
@@ -1,1 +1,20 @@
 console.log('About.js is running');
+
+const mainLeft = document.querySelector('#mainLeft');
+const mainRight = document.querySelector('#mainRight');
+const main = document.querySelector('#main');
+const instruction = document.querySelector('.instruction');
+instruction.setAttribute('transfered','false')
+
+mainLeft.addEventListener('click',() => {
+    if(!mainLeft.transferred) {
+        instruction.setAttribute('transfered',true)
+        console.log('click!')
+        mainLeft.style.animation = 'slideLeft 1.3s ease-in-out forwards';
+        main.innerHTML += '<div style="position: relative;display:flex;height:100%;width:100%;backgound-color:white;"><div>';
+        setTimeout(() => {
+            document.querySelector('#mainLeft > div').setAttribute('style','position:absolute;text-align:center;')
+            document.querySelector('#mainLeft > div').innerText = "Let me show you what I can do";
+        },900)
+    }
+})
