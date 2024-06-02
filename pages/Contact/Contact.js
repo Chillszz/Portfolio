6b64c0b701570270a01cb02d14ef41d6c27b0b28
CHillszz <jazperruedas@gmail.com>
2024-06-02 15:12:21 -0700

    testing contact page

--- pages/Contact/Contact.js
+++ pages/Contact/Contact.js
@@ -1,1 +1,33 @@
 console.log('Education.js is running')
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const formData = {
+        first_name: document.getElementById('firstname').value,
+        last_name: document.getElementById('lastname').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+
+    };
+
+    fetch('https://script.google.com/macros/s/AKfycbzwM0DyhW3OGVpiM3Rv41WM1jaPcsf9D0JzCWfSlijwbyYsyJ0C0ezKiYbSHyMTn_acJw/exec', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result === 'success') {
+            alert('Form submitted successfully!');
+        } else {
+            alert('Error submitting form');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error submitting form');
+    });
+});
